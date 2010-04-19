487c90cf077f61aedfa0539b9dd30f9176bd6d0a
malberti <>
2010-04-19 07:09:05 +0000

    filters fixed

--- Validation/EcalValidation/test/crab/ecalvalidationTEMPLATE_cfg.py
+++ Validation/EcalValidation/test/crab/ecalvalidationTEMPLATE_cfg.py
@@ -21,6 +21,8 @@ process.source = cms.Source("PoolSource",
     )
 )
 
+
+
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1000)
 )
@@ -35,20 +37,14 @@ process.skimming = cms.EDFilter("PhysDecl",
 process.load('L1TriggerConfig.L1GtConfigProducers.L1GtTriggerMaskTechTrigConfig_cff')
 process.load('HLTrigger/HLTfilters/hltLevel1GTSeed_cfi')
 process.hltLevel1GTSeed.L1TechTriggerSeeding = cms.bool(True)
-process.hltLevel1GTSeed.L1SeedsLogicalExpression = cms.string('0 AND (40 OR 41) AND NOT (36 OR 37 OR 38 OR 39) AND NOT ((42 AND NOT 43) OR (43 AND NOT 42))')
+process.hltLevel1GTSeed.L1SeedsLogicalExpression = cms.string('(40 OR 41) AND NOT (36 OR 37 OR 38 OR 39)')
 
-# Good Vertex Filter (Malgeri)
-#process.primaryVertexFilter = cms.EDFilter("GoodVertexFilter",
-#   vertexCollection = cms.InputTag('offlinePrimaryVertices'),
-#   minimumNDOF = cms.uint32(4) ,
-#   maxAbsZ = cms.double(15),	
-#   maxd0 = cms.double(2)	
-#)
 
+#Good Vertex Filter (see GOODCOLL skim)
 process.primaryVertexFilter = cms.EDFilter("VertexSelector",
-   src = cms.InputTag("offlinePrimaryVertices"),
-   cut = cms.string("!isFake && ndof > 4 && abs(z) <= 15 && position.Rho <= 2"), 
-   filter = cms.bool(True)   
+  src = cms.InputTag("offlinePrimaryVertices"),
+  cut = cms.string("!isFake && ndof > 4 && abs(z) <= 15 && position.Rho <= 2"), 
+  filter = cms.bool(True)   
 )
 
 # FilterOutScraping
@@ -60,9 +56,6 @@ process.noscraping = cms.EDFilter("FilterOutScraping",
 )
 
 
-process.goodvertex=cms.Sequence(process.primaryVertexFilter*process.noscraping)
-process.goodcollisions=cms.Sequence(process.hltLevel1GTSeed+process.goodvertex)
-
 process.load("Validation.EcalValidation.ecalvalidation_cfi")
 
 process.TFileService = cms.Service("TFileService",
@@ -70,8 +63,10 @@ process.TFileService = cms.Service("TFileService",
 )
 
 process.p = cms.Path(
-   #    process.skimming*
-   process.goodcollisions*
-   process.ecalvalidation
-   )
+    process.skimming*
+    process.hltLevel1GTSeed*
+    process.noscraping*
+    process.primaryVertexFilter*
+    process.ecalvalidation
+    )
 
